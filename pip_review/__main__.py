d617e3ec52d08b55c6cb2bf8af9d9622a29b254b
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-08-21 22:37:05 +0200

    Simplify set creation of LIST_ONLY and INSTALL_ONLY

--- pip_review/__main__.py
+++ pip_review/__main__.py
@@ -50,10 +50,58 @@ Python>=3.3.
 '''
 
 # parameters that pip list supports but not pip install
-LIST_ONLY = set('l local path format not-required exclude-editable include-editable'.split())
+LIST_ONLY = set(
+    [
+        'l',
+        'local',
+        'path',
+        'format',
+        'not-required',
+        'exclude-editable',
+        'include-editable',
+    ]
+)
 
 # parameters that pip install supports but not pip list
-INSTALL_ONLY = set('c constraint no-deps t target platform python-version implementation abi root prefix b build src U upgrade upgrade-strategy force-reinstall I ignore-installed ignore-requires-python no-build-isolation use-pep517 install-option global-option compile no-compile no-warn-script-location no-warn-conflicts no-binary only-binary prefer-binary no-clean require-hashes progress-bar'.split())
+INSTALL_ONLY = set(
+    [
+        'c',
+        'constraint',
+        'no-deps',
+        't',
+        'target',
+        'platform',
+        'python-version',
+        'implementation',
+        'abi',
+        'root',
+        'prefix',
+        'b',
+        'build',
+        'src',
+        'U',
+        'upgrade',
+        'upgrade-strategy',
+        'force-reinstall',
+        'I',
+        'ignore-installed',
+        'ignore-requires-python',
+        'no-build-isolation',
+        'use-pep517',
+        'install-option',
+        'global-option',
+        'compile',
+        'no-compile',
+        'no-warn-script-location',
+        'no-warn-conflicts',
+        'no-binary',
+        'only-binary',
+        'prefer-binary',
+        'no-clean',
+        'require-hashes',
+        'progress-bar',
+    ]
+)
 
 # command that sets up the pip module of the current Python interpreter
 PIP_CMD = [sys.executable, '-m', 'pip']
