c468790687fa752fde4348fa335c37915bff0a34
shouzy <82171453+realshouzy@users.noreply.github.com>
2024-01-27 00:41:00 +0100

    Restructure project to a single-file format

--- pip_review/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Run module as file."""
-from __future__ import annotations
-
-from pip_review._main import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
