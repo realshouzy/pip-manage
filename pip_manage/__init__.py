79a65c9f114326fa1e38df5d9e08176e0e7b8a27
shouzy <82171453+realshouzy@users.noreply.github.com>
2024-07-13 23:34:45 +0200

    Bump version to 1.1.2

--- pip_manage/__init__.py
+++ pip_manage/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__: Final[str] = "1.1.1"
+__version__: Final[str] = "1.1.2"
 __title__: Literal["pip-manage"] = "pip-manage"
 
 from typing import Final, Literal
